60e6a51e727c49380045fedb08a3ac74c0a616d2
tsunakit99 <tainosuke0926@gmail.com>
2025-04-15 01:33:42 +0900

    add:  CreateTaskのインターフェースを定義

--- backend/usecase/task/intreface.go
+++ backend/usecase/task/intreface.go
@@ -1,5 +1,13 @@
 package task
 
+import "time"
+
+type CreateTaskInput struct {
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	DueDate     *time.Time `json:"due_date"`
+}
 type TaskUsecase interface {
 	GetTasks(userID string) ([]TaskDTO, error)
+	CreateTask(userID string, input CreateTaskInput) error
 }
