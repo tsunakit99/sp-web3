ecc0cca4bb1501c7de4a688636f8a13de080340f
tsunakit99 <tainosuke0926@gmail.com>
2025-04-15 01:32:06 +0900

    add: ドメインモデルにフィールドを追加

--- backend/domain/task.go
+++ backend/domain/task.go
@@ -1,8 +1,13 @@
 package domain
 
+import "time"
+
 type Task struct {
 	ID          string
+	UserID      string
 	Title       string
 	Description string
 	IsCompleted bool
+	DueDate     *time.Time
+	CreatedAt   time.Time
 }
