b4ffbfe419a5ef26fcf0ba0a8cf169d936cf21de
tsunakit99 <tainosuke0926@gmail.com>
2025-04-16 03:14:25 +0900

    add: リポジトリインターフェースにUpdateメソッドを定義

--- backend/repository/task_repository.go
+++ backend/repository/task_repository.go
@@ -5,4 +5,7 @@ import "github.com/tsunakit99/sp-web3/domain"
 type TaskRepository interface {
 	FindByUserID(userID string) ([]domain.Task, error)
 	Insert(task domain.Task) error
+	Update(task domain.Task) error
+	// Delete(id string) error
+	// ToggleCompleted(id string, userID string) error
 }
