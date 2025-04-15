d157c3e8eb1dcb6adb470e0f08c4039d7460e0cb
tsunakit99 <tainosuke0926@gmail.com>
2025-04-16 03:15:47 +0900

    add: ユースケースのインターフェースにUpdateTaskメソッドを定義

--- backend/usecase/task/intreface.go
+++ backend/usecase/task/intreface.go
@@ -7,7 +7,15 @@ type CreateTaskInput struct {
 	Description string     `json:"description"`
 	DueDate     *time.Time `json:"due_date"`
 }
+
+type UpdateTaskInput struct {
+	ID          string     `json:"id"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	DueDate     *time.Time `json:"due_date"`
+}
 type TaskUsecase interface {
 	GetTasks(userID string) ([]TaskDTO, error)
 	CreateTask(userID string, input CreateTaskInput) error
+	UpdateTask(userID string, input UpdateTaskInput) error
 }
